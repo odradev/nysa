9350b0839caa3be210106c3df9c6986988c71db1
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2023-07-31 13:17:50 +0200

    Fix to_snake_case_ident

--- nysa/src/utils.rs
+++ nysa/src/utils.rs
@@ -2,5 +2,32 @@ use convert_case::{Case, Casing};
 use quote::format_ident;
 
 pub fn to_snake_case_ident(name: &str) -> proc_macro2::Ident {
-    format_ident!("{}", name.to_case(Case::Snake))
+    format_ident!("{}", to_snake_case(name))
+}
+
+pub fn to_snake_case(input: &str) -> String {
+    if input.starts_with('_') {
+        // `to_case()` consumes the heading `_`
+        format!("_{}", input.to_case(Case::Snake))
+    } else {
+        format!("{}", input.to_case(Case::Snake))
+    }
+}
+
+#[cfg(test)]
+mod t {
+    use proc_macro2::{Ident, Span};
+    use crate::utils::to_snake_case_ident;
+
+    #[test]
+    fn to_snake_case_ident_works() {
+        assert_eq!(ident("my_value"), to_snake_case_ident("MyValue"));
+        assert_eq!(ident("value"), to_snake_case_ident("value"));
+        assert_eq!(ident("value"), to_snake_case_ident("Value"));
+        assert_eq!(ident("_value"), to_snake_case_ident("_value"));
+    }
+
+    fn ident(string: &str) -> Ident {
+        Ident::new(string, Span::call_site())
+    }
 }
